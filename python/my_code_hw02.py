3c6ab18c861058091ab11a62fe5b1a32bbef5d47
Ondrej Vesely <ondrej-vesely@seznam.cz>
2020-12-17 00:37:39 +0100

    radius pixels (shit algo)

--- python/my_code_hw02.py
+++ python/my_code_hw02.py
@@ -20,8 +20,26 @@ def create_circle_exterior_mask(h, w, center, radius):
 def create_circle_outline_mask(h, w, center, radius, thickness=math.sqrt(2)):
     Y, X = np.ogrid[:h, :w]
     dist_from_center = np.sqrt((X - center[0])**2 + (Y-center[1])**2)
-    mask = (radius-thickness) < dist_from_center <= radius
+    mask = np.logical_and( (radius-thickness) < dist_from_center, 
+                            dist_from_center <= radius )
     return mask
+
+def bresenham_line(x1, y1, x2, y2):  
+    """Return tuples of coords on bresenham line"""
+    result = []
+    m_new = 2 * (y2 - y1)  
+    slope_error_new = m_new - (x2 - x1) 
+    y=y1 
+    for x in range(x1,x2+1):  
+        result.append((x,y))
+        # Add slope to increment angle formed  
+        slope_error_new =slope_error_new + m_new  
+        # Slope error reached limit, time to  
+        # increment y and update slope error.  
+        if (slope_error_new >= 0):  
+            y=y+1
+            slope_error_new =slope_error_new - 2 * (x2 - x1) 
+
 
 def output_viewshed(d, viewpoints, maxdistance, output_file):
     """
@@ -55,25 +73,31 @@ def output_viewshed(d, viewpoints, maxdistance, output_file):
     v = viewpoints[0]
     #-- index of this point in the numpy raster
     vrow, vcol = d.index(v[0], v[1])
-    #-- put that pixel with value 2
-    output[vrow, vcol] = 2
 
-    # set points outside of radius to value 3
-    mask_union = True
+
+    # set pixels outside of radius to value 1
+    radius_mask = False
+    for v in viewpoints:
+        vrow, vcol = d.index(v[0], v[1])
+        mask = create_circle_outline_mask(d.shape[0], d.shape[1], 
+                                          (vcol, vrow), maxdistance/pixel_size)
+        radius_mask = np.logical_or(mask, radius_mask)      
+    np.putmask(output, radius_mask, 1)
+
+    # set pixels outside of radius to value 3
+    exterior_mask = True
     for v in viewpoints:
         vrow, vcol = d.index(v[0], v[1])
         mask = create_circle_exterior_mask(d.shape[0], d.shape[1], 
                                           (vcol, vrow), maxdistance/pixel_size)
-        mask_union = np.logical_and(mask, mask_union)      
-    np.putmask(output, mask_union, 3)
+        exterior_mask = np.logical_and(mask, exterior_mask)      
+    np.putmask(output, exterior_mask, 3)
 
-    # set viewpoint pixel to value 2
-    for v in viewpoints:
+    # set viewpoint pixels to value 2
+    for v in viewpoints:  
         vrow, vcol = d.index(v[0], v[1])
         output[vrow, vcol] = 2
 
-
-    
     # finally write numpy raster array as tif
     with rasterio.open(output_file, 'w', 
                        driver='GTiff', 
@@ -89,22 +113,22 @@ def output_viewshed(d, viewpoints, maxdistance, output_file):
 
 
 
-# def Bresenham_with_rasterio():
-#     # d = rasterio dataset as above
-#     a = (10, 10)
-#     b = (100, 50)
-#     #-- create in-memory a simple GeoJSON LineString
-#     v = {}
-#     v["type"] = "LineString"
-#     v["coordinates"] = []
-#     v["coordinates"].append(d.xy(a[0], a[1]))
-#     v["coordinates"].append(d.xy(b[0], b[1]))
-#     shapes = [(v, 1)]
-#     re = features.rasterize(shapes, 
-#                             out_shape=d.shape, 
-#                             # all_touched=True,
-#                             transform=d.transform)
-#     # re is a numpy with d.shape where the line is rasterised (values != 0)
+def Bresenham_with_rasterio():
+    # d = rasterio dataset as above
+    a = (10, 10)
+    b = (100, 50)
+    #-- create in-memory a simple GeoJSON LineString
+    v = {}
+    v["type"] = "LineString"
+    v["coordinates"] = []
+    v["coordinates"].append(d.xy(a[0], a[1]))
+    v["coordinates"].append(d.xy(b[0], b[1]))
+    shapes = [(v, 1)]
+    re = features.rasterize(shapes, 
+                            out_shape=d.shape, 
+                            all_touched=True,
+                            transform=d.transform)
+    # re is a numpy with d.shape where the line is rasterised (values != 0)
 
 
 
