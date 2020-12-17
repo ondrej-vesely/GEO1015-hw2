0467d90a9b82bd6654412c0b97dbf87bd79a5fbf
Ondrej Vesely <ondrej-vesely@seznam.cz>
2020-12-17 13:08:29 +0100

    sort of working

--- python/my_code_hw02.py
+++ python/my_code_hw02.py
@@ -1,9 +1,9 @@
 #-- my_code_hw02.py
-#-- Assignment 02 GEO1015.2020
-#-- Ondrej Vesely 
+# -- Assignment 02 GEO1015.2020
+# -- Ondrej Vesely
 #-- 5162130
-#-- Guilherme Spinoza Andreo 
-#-- 5383994 
+# -- Guilherme Spinoza Andreo
+#-- 5383994
 
 import sys
 import math
@@ -11,26 +11,14 @@ import numpy as np
 import rasterio
 from rasterio import features
 
-def dist_matrix(h, w, center):
+def circle_exterior_mask(h, w, center, radius):
     Y, X = np.ogrid[:h, :w]
     dist_from_center = np.sqrt((X - center[0])**2 + (Y-center[1])**2)
-    # get rid of zero distance
-    dist_from_center[center[1], center[0]] = 0.0000001
-    return dist_from_center
-
-def circle_exterior_mask(h, w, center, radius):
-    dist_from_center = dist_matrix(h, w, center)
     mask = dist_from_center > radius
     return mask
 
-def circle_outline_mask(h, w, center, radius, thickness=math.sqrt(2)):
-    dist_from_center = dist_matrix(h, w, center)
-    mask = np.logical_and( (radius-thickness) < dist_from_center, 
-                            dist_from_center <= radius )
-    return mask
-
 def bresenham_line_mask(h, w, x1, y1, x2, y2):
-    l = [({"type":"LineString", "coordinates":[(x1, y1), (x2, y2)]}, 1)]
+    l = [({"type": "LineString", "coordinates": [(x1, y1), (x2, y2)]}, 1)]
     mask = features.rasterize(l, out_shape=(h, w), all_touched=True)
     mask[x2, y2] = 1
     return mask
@@ -48,13 +36,13 @@ def bresenham_circle_coords(x0, y0, radius):
     coords.add((x0 + radius, y0))
     coords.add((x0 - radius, y0))
     while x < y:
-        if f >= 0: 
+        if f >= 0:
             y -= 1
             ddf_y += 2
             f += ddf_y
         x += 1
         ddf_x += 2
-        f += ddf_x    
+        f += ddf_x
         coords.add((x0 + x, y0 + y))
         coords.add((x0 - x, y0 + y))
         coords.add((x0 + x, y0 - y))
@@ -65,111 +53,84 @@ def bresenham_circle_coords(x0, y0, radius):
         coords.add((x0 - y, y0 - x))
     return coords
 
+
 def output_viewshed(d, viewpoints, maxdistance, output_file):
     """
     !!! TO BE COMPLETED !!!
-     
+
     Function that writes the output raster
-     
+
     Input:
         d:            the input datasets (rasterio format)  
         viewpoints:   a list of the viewpoints (x, y, height)
         maxdistance:  max distance one can see
         output_file:  path of the file to write as output
-        
+
     Output:
         none (but output GeoTIFF file written to 'output-file')
-    """  
-    
-    # [this code can and should be removed/modified/reutilised]
-    # [it's just there to help you]
-
+    """
     # raster of the input
-    terrain  = d.read(1)
+    terrain = d.read(1)
     # raster of the output
     output = np.zeros(d.shape, dtype=np.int8)
     # get dimensions in raster space
     pixel_size = d.transform[0]
     pixel_radius = maxdistance/pixel_size
-
-
-
+    
+    
     for v in viewpoints:
-        # get raster space coords and height
+        # get viewpoint coords and height
         vrow, vcol = d.index(v[0], v[1])
         vheight = terrain[vrow, vcol] + v[2]
 
-        # precalculate distance matrix
-        distance_matrix = dist_matrix(*d.shape, (vcol, vrow))
-        # # precalculate tangent matrix
-        tangent_matrix = (terrain - vheight) / distance_matrix
-
-        # get points on circle
-        cpoints = bresenham_circle_coords(vcol, vrow, pixel_radius)
-        # try line drawing
-        y1, x1 = vcol, vcol
-        for x2, y2 in cpoints:
+        # get points on circle circumference
+        # and draw a bresenham lines towards them
+        x1, y1 = vcol, vrow
+        for x2, y2 in bresenham_circle_coords(x1, y1, pixel_radius+1):
             line = bresenham_line_mask(*d.shape, x1, y1, x2, y2)
-            np.putmask(output, line, 4)
+            
+            # get each lines points, their distance and sort them
+            y, x = np.where(line)
+            dist = np.sqrt((x - x1)**2 + (y-y1)**2)
+            order = dist.argsort()
+            x, y, dist = x[order], y[order], dist[order]
 
-   
-   
-   
-    # set pixels outside of radius to value 5
-    radius_mask = False
+            # calculate each points tangent
+            dist[0] = 0.0001  # get rid of zero div
+            tang = (terrain[y, x] - vheight) / dist 
+            
+            # apply tangent line of sight algo
+            max_t = float('-inf')
+            for i, t in enumerate(tang):
+                if t >= max_t:
+                    output[y[i],x[i]] = tang
+                    max_t = t
+
+
+    # set viewpoint pixels to value 2
     for v in viewpoints:
         vrow, vcol = d.index(v[0], v[1])
-        mask = circle_outline_mask  (d.shape[0], d.shape[1], 
-                                    (vcol, vrow), pixel_radius)
-        radius_mask = np.logical_or(mask, radius_mask)      
-    np.putmask(output, radius_mask, 5)
-
-
+        output[vrow, vcol] = 2
+    
     # set pixels outside of radius to value 3
     exterior_mask = True
     for v in viewpoints:
         vrow, vcol = d.index(v[0], v[1])
-        mask = circle_exterior_mask (d.shape[0], d.shape[1], 
+        mask = circle_exterior_mask(d.shape[0], d.shape[1],
                                     (vcol, vrow), pixel_radius)
-        exterior_mask = np.logical_and(mask, exterior_mask)      
+        exterior_mask = np.logical_and(mask, exterior_mask)
     np.putmask(output, exterior_mask, 3)
 
-    # set viewpoint pixels to value 2
-    for v in viewpoints:  
-        vrow, vcol = d.index(v[0], v[1])
-        output[vrow, vcol] = 2
-
+ 
     # finally write numpy raster array as tif
-    with rasterio.open(output_file, 'w', 
-                       driver='GTiff', 
+    with rasterio.open(output_file, 'w',
+                       driver='GTiff',
                        height=terrain.shape[0],
-                       width=terrain.shape[1], 
-                       count=1, 
+                       width=terrain.shape[1],
+                       count=1,
                        dtype=rasterio.uint8,
-                       crs=d.crs, 
+                       crs=d.crs,
                        transform=d.transform) as dst:
         dst.write(output.astype(rasterio.uint8), 1)
 
     print("Viewshed file written to '%s'" % output_file)
-
-
-
-def Bresenham_with_rasterio():
-    # d = rasterio dataset as above
-    a = (10, 10)
-    b = (100, 50)
-    #-- create in-memory a simple GeoJSON LineString
-    v = {}
-    v["type"] = "LineString"
-    v["coordinates"] = []
-    v["coordinates"].append(d.xy(a[0], a[1]))
-    v["coordinates"].append(d.xy(b[0], b[1]))
-    shapes = [(v, 1)]
-    re = features.rasterize(shapes, 
-                            out_shape=d.shape, 
-                            all_touched=True,
-                            transform=d.transform)
-    # re is a numpy with d.shape where the line is rasterised (values != 0)
-
-
-
